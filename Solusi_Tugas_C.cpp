a8aacf09d0b4d6a11f00a247b2a181979973a4eb
JovanBagasPangestu <pjovanbagas@gmail.com>
2025-03-05 00:51:21 +0800

    Update Solusi_Tugas_C.cpp

--- Solusi_Tugas_C.cpp
+++ Solusi_Tugas_C.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+using namespace std;
+
+int pangkat(int a, int b) {
+    if (b == 0) 
+        return 1; 
+    else 
+        return a * pangkat(a, b - 1); 
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a, b;
+    cout << "Masukkan bilangan (a): ";
+    cin >> a;
+    cout << "Masukkan pangkat (b): ";
+    cin >> b;
+
+    int hasil = pangkat(a, b);
+    cout << a << "^" << b << " = " << hasil << endl;
+
     return 0;
 }
