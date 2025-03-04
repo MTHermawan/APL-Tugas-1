b9626da37d2f19c89b868fcd77805144b91d9d8f
MuhammadRivaFachrodhiya <muhammadrivaf@gmail.com>
2025-03-04 22:44:34 +0800

    Solusi_Tugas_B.py

--- Solusi_Tugas_B.cpp
+++ Solusi_Tugas_B.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+def cek_tripel_pythagoras(a, b, c):
+    sisi = sorted([a, b, c])
+    x, y, z = sisi
+    print(f"\nMenguji apakah {z}² = {x}² + {y}²")
+    print(f"{z**2} = {x**2} + {y**2}")
+    print(f"{z**2} = {x**2 + y**2}")
+    if z**2 == x**2 + y**2:
+        print(f"{x}, {y}, dan {z} membentuk Tripel Pythagoras!")
+    else:
+        print(f"{x}, {y}, dan {z} BUKAN Tripel Pythagoras.")
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+a = int(input("Masukkan nilai a: "))
+b = int(input("Masukkan nilai b: "))
+c = int(input("Masukkan nilai c: "))
+    
+cek_tripel_pythagoras(a, b, c)
