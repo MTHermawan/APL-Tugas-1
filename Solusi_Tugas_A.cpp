aa793d735693a4cd23c7ccf1974624b6a92282f6
orkvacy <krotago@gmail.com>
2025-03-05 02:06:22 +0800

    ganti tipe data jadi float

--- Solusi_Tugas_A.cpp
+++ Solusi_Tugas_A.cpp
@@ -1,34 +1,31 @@
 #include <iostream>
-#include <cmath> // pow buat pangkat
+#include <cmath> 
 
 using namespace std;
 
-void jenisSegitiga(int sisi1, int sisi2, int sisi3) {
-    //cek apakah segitiga
+void jenisSegitiga(float sisi1, float sisi2, float sisi3) {
     if ((sisi1 + sisi2 > sisi3) && (sisi1 + sisi3 > sisi2) && (sisi2 + sisi3 > sisi1)) {
-        //segitiga sama kaki
+        //cek segitiga sama kaki
         if ((sisi1 == sisi2) || (sisi1 == sisi3) || (sisi2 == sisi3)) {
             cout << "Segitiga Sama Kaki" << endl;
         }
-        //segitiga siku siku
-        else if ((pow(sisi1, 2) + pow(sisi2, 2) == pow(sisi3, 2)) ||
-                (pow(sisi1, 2) + pow(sisi3, 2) == pow(sisi2, 2)) ||
-                (pow(sisi2, 2) + pow(sisi3, 2) == pow(sisi1, 2))) {
+        //cek segitiga siku-siku 
+        else if (fabs(pow(sisi1, 2) + pow(sisi2, 2) - pow(sisi3, 2)) < 0.0001 ||
+                 fabs(pow(sisi1, 2) + pow(sisi3, 2) - pow(sisi2, 2)) < 0.0001 ||
+                 fabs(pow(sisi2, 2) + pow(sisi3, 2) - pow(sisi1, 2)) < 0.0001) {
             cout << "Segitiga Siku-Siku" << endl;
         }
-        //jika bukan sama kaki atau siku siku
         else {
             cout << "Segitiga Sembarang" << endl;
         }
     } else {
-        cout << "Tidak dapat membentuk segitiga" << endl;
+        cout << "Tidak bisa membentuk segitiga" << endl;
     }
 }
 
 int main() {
-    int sisi1, sisi2, sisi3;
+    float sisi1, sisi2, sisi3;
     
-    //input dari user
     cout << "Masukkan sisi 1: ";
     cin >> sisi1;
     cout << "Masukkan sisi 2: ";
