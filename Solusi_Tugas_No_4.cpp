8986aaf6ae6ef4f992871b0a776d64a74e6ee097
MuhammadRivaFachrodhiya <muhammadrivaf@gmail.com>
2025-03-04 23:40:24 +0800

    Update Solusi_Tugas_No_4.cpp

--- Solusi_Tugas_No_4.cpp
+++ Solusi_Tugas_No_4.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int angka = 10; 
-    int *PointerAngka = &angka; 
 
-    cout << "Nilai angka: " << angka << endl;
-    cout << "Alamat angka: " << &angka << endl;
-    cout << "Nilai yang ditunjuk oleh pointer: " << *PointerAngka << endl;
+// Fungsi untuk mengubah nilai menggunakan pointer
+void ubahNilai(int *ptr) {
+    *ptr = 20; // Mengubah nilai yang ditunjuk oleh pointer
+}
+
+int main() {
+    int Angka = 10;
+    int *PointerAngka = &Angka;
+    cout << "Sebelum perubahan: " << Angka << endl;
+    ubahNilai(PointerAngka); // Memanggil fungsi dengan pointer
+    cout << "Setelah perubahan: " << Angka << endl;
     return 0;
 }
