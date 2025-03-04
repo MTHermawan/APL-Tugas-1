e43efc987c7b2d2418583f873e31f8426788dc90
MuhammadRivaFachrodhiya <muhammadrivaf@gmail.com>
2025-03-04 22:43:36 +0800

    Solusi_Tugas_No_4.cpp

--- Solusi_Tugas_No_4.cpp
+++ Solusi_Tugas_No_4.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int angka = 10; 
+    int *PointerAngka = &angka; 
+
+    cout << "Nilai angka: " << angka << endl;
+    cout << "Alamat angka: " << &angka << endl;
+    cout << "Nilai yang ditunjuk oleh pointer: " << *PointerAngka << endl;
     return 0;
 }
